a1d0991afd5cd7f03161693fc9489e1cac171a43
Viet Anh Tran <viet.anhtran@myob.com>
2017-12-12 17:29:32 +1300

    Test for 3by 5 field

--- MineSweeperKata/Test/MineAnalyserShould.cs
+++ MineSweeperKata/Test/MineAnalyserShould.cs
@@ -7,9 +7,9 @@ namespace MineSweeperKata.Test
     public class MineAnalyserShould
     {
         [Test]
-        public void DoSomething()
+        public void Read4By4Field()
         {
-            var blah = new MineAnalyser();
+            var mineAnalyser = new MineAnalyser();
             var field = new Field()
             {
                 NoOfRows = 4,
@@ -19,11 +19,30 @@ namespace MineSweeperKata.Test
                         "..*." +
                         "...."
             };
-            var actual = blah.ReadMine(field);
+            var actual = mineAnalyser.ReadMine(field);
             const string expected = "1110" +
                                     "1*21" +
                                     "12*1" +
                                     "0111";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Read3By5Field()
+        {
+            var mineAnalyser = new MineAnalyser();
+            var field = new Field()
+            {
+                NoOfRows = 3,
+                NoOfColumns = 5,
+                Value = "**..." +
+                        "....." +
+                        ".*..."
+            };
+            var actual = mineAnalyser.ReadMine(field);
+            const string expected = "**100" +
+                                    "33200" +
+                                    "1*100";
             Assert.AreEqual(expected, actual);
         }
     }
