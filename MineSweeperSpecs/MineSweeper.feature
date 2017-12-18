f96542a0af5afb51980806746ddab5faca4286a3
Viet Anh Tran <viet.anhtran@myob.com>
2017-12-18 16:51:51 +1300

    Set up specflow test for sweeping a four by four field

---
+++ MineSweeperSpecs/MineSweeper.feature
@@ -0,0 +1,9 @@
+﻿Feature: MineSweeper
+	In order to master MineSweeper game
+	I want to understand how numbers in each square are calculated for a single field
+
+@mytag
+Scenario: Sweep a four by four field
+	Given I have a four by four field
+	When the MineSweeper sweep the field
+	Then the result should be a field that each square would display the number of surrounding bombs
---
+++ MineSweeperSpecs/MineSweeperSteps.cs
@@ -0,0 +1,45 @@
+﻿using MineSweeperKata;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace MineSweeperSpecs
+{
+    [Binding]
+    public class MineSweeperSteps
+    {
+        private string _rawField;
+        private string _result;
+        private readonly MineSweeper _mineSweeper = new MineSweeper();
+        [Given(@"I have a four by four field")]
+        public void GivenIHaveAFourByFourField()
+        {
+            _rawField = "~~~\n" +
+                        "44\n" +
+                        "*...\n" +
+                        "....\n" +
+                        ".*..\n" +
+                        "....\n" +
+                        "~~~";
+        }
+        
+        [When(@"the MineSweeper sweep the field")]
+        public void WhenTheMineSweeperSweepTheField()
+        {
+            _result = _mineSweeper.Sweep(_rawField);
+        }
+        
+        [Then(@"the result should be a field that each square would display the number of surrounding bombs")]
+        public void ThenTheResultShouldBeAFieldThatEachSquareWouldDisplayTheNumberOfSurroundingBombs()
+        {
+            const string expected = "~~~\n" +
+                                    "Field #1:\n" +
+                                    "*100\n" +
+                                    "2210\n" +
+                                    "1*10\n" +
+                                    "1110\n" +
+                                    "~~~\n" +
+                                    "\n";
+            Assert.AreEqual(expected, _result);
+        }
+    }
+}
